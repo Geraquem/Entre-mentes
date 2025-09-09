ee228e3a62fb4f7c484adc1d8e8709b4b4a3c175
gers <geraquem@gmail.com>
2025-09-09 11:41:25 +0200

    wip

--- app/src/main/java/com/mmfsin/betweenminds/presentation/menu/MenuFragment.kt
+++ app/src/main/java/com/mmfsin/betweenminds/presentation/menu/MenuFragment.kt
@@ -1,15 +1,24 @@
 package com.mmfsin.betweenminds.presentation.menu
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Context
+import android.content.Context.BLUETOOTH_SERVICE
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.viewModels
-import com.marcinmoskala.arcseekbar.ProgressListener
 import com.mmfsin.betweenminds.base.BaseFragment
 import com.mmfsin.betweenminds.databinding.FragmentMenuBinding
 import com.mmfsin.betweenminds.presentation.MainActivity
+import com.mmfsin.betweenminds.utils.checkNotNulls
 import com.mmfsin.betweenminds.utils.showErrorDialog
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,30 +28,83 @@ class MenuFragment : BaseFragment<FragmentMenuBinding, MenuViewModel>() {
     override val viewModel: MenuViewModel by viewModels()
     private lateinit var mContext: Context
 
+    private var bluetoothManager: BluetoothManager? = null
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    var scanner: BluetoothLeScanner? = null
+
     override fun inflateView(
         inflater: LayoutInflater, container: ViewGroup?
     ) = FragmentMenuBinding.inflate(inflater, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        instanceValues()
+
+        if (canUseBle()) {
+            scanDevices()
+        }
+    }
+
+    private fun instanceValues() {
+        bluetoothManager = activity?.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapter = bluetoothManager?.adapter
+        scanner = bluetoothAdapter?.bluetoothLeScanner
+    }
+
+    private fun canUseBle(): Boolean {
+        var usable = false
+        checkNotNulls(bluetoothAdapter, activity?.packageManager) { _, pm ->
+            if (pm.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+                usable = true
+            }
+        }
+        return usable
+    }
+
+    private fun scanDevices() {
+        val scanCallback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                super.onScanResult(callbackType, result)
+                val device = result.device
+                if (ActivityCompat.checkSelfPermission(
+                        mContext,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+
+                println("-------------------------------------")
+                println("Encontrado: ${device.name} - ${device.address}")
+                println("-------------------------------------")
+            }
+
+            override fun onBatchScanResults(results: List<ScanResult>) { }
+            override fun onScanFailed(errorCode: Int) { }
+        }
+
+        scanner?.startScan(scanCallback)
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            scanner.stopScan(scanCallback)
+//        }, 10000)
     }
 
     override fun setUI() {
-        binding.apply {
-            slider.addOnChangeListener { _, value, _ ->
-                valueText.text = "${value.toInt()}"
-            }
+        binding.apply { }
 
-            seekArc.onProgressChangedListener = ProgressListener { progress ->
-                val value = progress - 100   // convierte 0..200 -> -100..100
-                valueText.text = "$value"
-            }
-        }
+
     }
 
     override fun setListeners() {
-        binding.apply {
-        }
+        binding.apply { }
     }
 
     override fun observe() {
